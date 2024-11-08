b43558408a426c19426f593c29e591a6b97e30e6
raju12208160 <mailraju1406@gmail.com>
2024-11-08 18:56:22 +0530

    Initial commit

---
+++ CarRent/agency/addcar_process1.php
@@ -0,0 +1,74 @@
+<?php
+include('../config/session_agency.php');
+
+function GetImageExtension($imagetype) {
+    if (empty($imagetype)) return false;
+
+    switch ($imagetype) {
+        case 'image/bmp': return '.bmp';
+        case 'image/gif': return '.gif';
+        case 'image/jpeg': return '.jpg';
+        case 'image/png': return '.png';
+        default: return false;
+    }
+}
+
+$car_model = $conn->real_escape_string($_POST['car_model']);
+$car_number = $conn->real_escape_string($_POST['car_number']);
+$seating_cap = $conn->real_escape_string($_POST['seating_capacity']);
+$rent_per_day = $conn->real_escape_string($_POST['rent_per_day']);
+$car_availability = "yes"; // Set car availability to "yes"
+
+$check_query = "SELECT car_number FROM cars WHERE car_number = '$car_number'";
+$result = $conn->query($check_query);
+if ($result->num_rows > 0) {
+    header('Location: addcar.php?success=Car with this car number already exists. Please add a different car number.');
+    exit;
+} else {
+    if (!empty($_FILES["car_image"]["name"])) {
+        $file_name = $_FILES["car_image"]["name"];
+        $temp_name = $_FILES["car_image"]["tmp_name"];
+        $imgtype = $_FILES["car_image"]["type"];
+        $ext = GetImageExtension($imgtype);
+        $imagename = $file_name;
+        
+        // Define the relative path for storing and displaying the image
+        $relative_path = "/Car-Rental-System-main/CarRent/uploads/" . $imagename;
+        $absolute_path = $_SERVER['DOCUMENT_ROOT'] . $relative_path; // Full server path for move_uploaded_file
+
+        // Move the uploaded file to the target directory
+        if (move_uploaded_file($temp_name, $absolute_path)) {
+            // Save only the relative path in the database
+            $query = "INSERT INTO cars (car_model, car_number, seating_cap, rent_per_day, car_img, car_available) 
+                      VALUES ('$car_model', '$car_number', '$seating_cap', '$rent_per_day', '$relative_path', '$car_availability')";
+            $success = $conn->query($query);
+
+            if ($success) {
+                // Retrieve the car_id from the newly inserted car
+                $car_id = $conn->insert_id;
+
+                // Insert data into agencycars table
+                $agency_username = $_SESSION['login_agency'];
+                $query2 = "INSERT INTO agencycar (car_id, agency_username) VALUES ('$car_id', '$agency_username')";
+                $success2 = $conn->query($query2);
+            }
+        }
+    }
+}
+
+if (isset($success) && $success) {
+    ?>
+    <div class="message_container">
+        <div class="jumbotron" style="text-align: center;">
+            Car added successfully
+            <?php echo $conn->error; ?>
+            <br><br>
+            <a href="addcar.php" class="btn btn-default">Go Back</a>
+        </div>
+    </div>
+    <?php
+    exit;
+}
+
+$conn->close();
+?>
